e85a5bbe6ab308922bba244a35605e81896ae2df
Oriol Angelet <uriaeri@gmail.com>
2023-03-28 18:08:09 +0200

    first commit

---
+++ MVC-MVVM/MVC-MVVM/MVVM/Model/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  MVC-MVVM
+//  https://youtu.be/6qUFa9asnN4
+//  Created by Uri on 28/3/23.
+//
+
+import Foundation
+
+struct Postmodel: Codable {
+    let user: Int
+    let id: Int
+    let title: String
+    let body: String
+}
---
+++ MVC-MVVM/MVC-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-MVVM
+//
+//  Created by Uri on 28/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
